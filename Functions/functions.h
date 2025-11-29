b6464e4d1c589e0cd38f2fe85138eb86953e0e61
Muhammad <bsse25002@itu.edu.pk>
2025-11-29 23:52:05 +0500

    Functions

---
+++ Functions/functions.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+//Including Libraries
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+using namespace std;
+
+#endif
